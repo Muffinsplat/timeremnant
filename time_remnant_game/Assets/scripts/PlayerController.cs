373ad3089c5d161c9656b5e9715528c4ac2d01c8
OskarCoolKid <37690227+OskarCoolKid@users.noreply.github.com>
2018-08-27 18:11:21 +0200

    added movement scripts
    nothing else..... tihi... excpet camera

---
+++ time_remnant_game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    public float rotationRate = 360;
+
+    public float movespeed = 2;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float trunAxis = Input.GetAxis(turnInputAxis);
+
+        ApplyInput(moveAxis, trunAxis);
+    }
+
+    private void ApplyInput(float moveInput,
+                            float turnInput)
+    {
+        Move(moveInput);
+        Turn(turnInput); 
+    }
+        
+        
+
+private void Move(float input)
+    {
+        transform.Translate(Vector3.forward * input * movespeed);
+    }
+
+    private void Turn(float input)
+    {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+}
