4ea8010da94b0dd2fdcdcf50d1407be1ca05e667
JonathanEngberg <34317404+zDuckLordz@users.noreply.github.com>
2018-09-01 15:03:38 +0200

    Main menu
    Nu kan man trycka på play, options och exit. ska bara lägga till inställningarna så man kan konfigurera grafik, musik o ljud, keybindings och mouse settings.

---
+++ time_remnant_game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Graphics()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Keybindings()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Sound()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void SettingsBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+}
