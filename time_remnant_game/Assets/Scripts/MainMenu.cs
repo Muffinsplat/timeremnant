feddf785115ae7f47359387a8e60fc2597bd3326
JonathanEngberg <34317404+zDuckLordz@users.noreply.github.com>
2018-10-10 12:28:46 +0200

    mAIN mENU
    Is WoRkInG nOw

--- time_remnant_game/Assets/Scripts/MainMenu.cs
+++ time_remnant_game/Assets/Scripts/MainMenu.cs
@@ -1,44 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
 	public void PlayGame()
 	{
-		SceneManager.LoadScene(1);
+		EditorSceneManager.LoadScene(1);
 	}
 
-    public void Settings()
-    {
-        SceneManager.LoadScene(2);
-    }
-
-    public void Graphics()
-    {
-        SceneManager.LoadScene(3);
-    }
-
-    public void Keybindings()
-    {
-        SceneManager.LoadScene(4);
-    }
-
-    public void Sound()
-    {
-        SceneManager.LoadScene(5);
-    }
-
-    public void SettingsBack()
-    {
-        SceneManager.LoadScene(0);
-    }
+	public void SettingsMenu()
+	{
+		EditorSceneManager.LoadScene(2);
+	}
+	
+	public void Back()
+	{
+		EditorSceneManager.LoadScene(0);
+	} 
 
 	public void QuitGame()
 	{
 		Application.Quit();
 	}
-
 }
